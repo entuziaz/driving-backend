d2db66b1d72718ddf19b79c7ed8227058a6778af
Jkaylight <jkaylight@gmail.com>
2020-07-18 12:58:32 +0100

    added Whitenoise setting for serving static files

--- drivingBackend/drivingBackend/settings.py
+++ drivingBackend/drivingBackend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -181,3 +182,8 @@ LOGGING = {
         },
     },
 }
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
